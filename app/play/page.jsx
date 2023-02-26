3b0ee6bc0e939922eb7193f8624fc1b7cdcf1d62
Mariano Silveyra <marianosilveyra@noduscompany.com>
2023-02-26 18:38:52 -0300

    feat: minor changes

--- app/play/page.jsx
+++ app/play/page.jsx
@@ -30,15 +30,15 @@ const createMoreFood = (limit, grid, setGrid, setMoreFood) => {
 const Snake = ({ top, left, grid, state }) => {
     const type = grid[left][top][0]
     const classNames = {
-        head: state === "loose" ? "bg-red-900 rounded opacity-75" : "bg-black-dark rounded-sm",
+        head: state === "loose" ? "bg-red-900 rounded opacity-75" : "bg-black-dark rounded",
         body: state === "loose" ? "bg-red-500 opacity-75" : "bg-green-700",
         food: (state === "playing" || state === "pause") ? "bg-red-700 border border-white !h-3 !w-3 rounded-full" : "",
     }
 
     return (
         <div
-            style={{ top: 15 * top, left: 15 * left }}
-            className={`transition absolute z-10 h-[15px] w-[15px] ${classNames[type] || ""}`}
+            style={{ top: 15 * top, left: 15 * left, transition: "all 0.2s" }}
+            className={`absolute z-10 h-[15px] w-[15px] ${classNames[type] || ""}`}
         />
     )
 }
