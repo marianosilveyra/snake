fd2263769cc8b480095ee7279201bde1620bfc6a
Mariano Silveyra <marianosilveyra@noduscompany.com>
2023-02-27 15:59:05 -0300

    feat: snake svg animation

--- app/components/Button/index.jsx
+++ app/components/Button/index.jsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion"
 export default function Button({ href, onCLick, text, disabled = false, className, animate }) {
     return (
         <motion.div
+            drag={animate}
+            dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
             animate={animate ? { scale: [1, 1.1, 1] } : {}}
             transition={animate ? { duration: 1, repeat: Infinity } : {}}
             className={`transition ease-in-out lg:delay-75 lg:hover:-translate-y-[2px] lg:hover:scale-105 duration-75 font-semibold text-center rounded-lg w-full ${
--- app/page.jsx
+++ app/page.jsx
@@ -3,22 +3,24 @@
 import links from "./components/Navigation/links"
 import Button from "./components/Button"
 import Snake from "./assets/svg/snake.jsx"
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 export default function HomePage() {
     return (
-        <motion.div
-            className="flex h-screen flex-col justify-center"
-            drag
-            dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
-        >
+        <div className="flex h-screen flex-col justify-center">
             <div className="self-center">
                 <h1 className="text-2xl font-bold text-center mb-6">Welcome to Snake</h1>
                 <Button animate={true} href={links[1].route} text="NEW GAME" className="bg-fucsia-default text-white" />
-                <div className="w-full h-36 mt-4 flex justify-center align-middle">
+                <motion.div
+                    drag
+                    dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+                    animate={{ rotate: [-360, 360] }}
+                    transition={{ duration: 4, repeat: Infinity }}
+                    className="w-full h-36 mt-4 flex justify-center align-middle"
+                >
                     <Snake />
-                </div>
+                </motion.div>
             </div>
-        </motion.div>
+        </div>
     )
 }
