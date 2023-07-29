2ecc762dde73b2a8c1cd39edccb7ecc5e0e185ec
Mariano Silveyra <marianosilveyra@noduscompany.com>
2023-07-29 09:23:09 -0300

    feat: clean up of db and related

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack", "react-svg-loader"],
-      });
-  
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "react-svg-loader"],
+    })
+
+    return config
+  },
+  experimental: {
+    allowMiddlewareResponseBody: true,
+  },
+}
+
+module.exports = nextConfig
--- utils/getScores.js
+++
@@ -1,9 +0,0 @@
-export const getScores = async () => {
-  try {
-    const response = await fetch("/api/getScoresApi")
-    const scoresData = await response.json()
-    return scoresData
-  } catch (error) {
-    console.error("An error occurred while fetching scores:", error)
-  }
-}
